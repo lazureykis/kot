42399b303f84cdcb36d13c4bda16ca22bc2ef49d
Pavel Lazureykis <lazureykis@gmail.com>
2015-02-24 12:18:27 +0300

    UPD deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,13 @@ set :deploy_to, '/var/www/kot'
 set :log_level, :info
 # set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
+
+set :nginx_server_name, 'kot.lazureykis.me'
 
 set :rvm_type, :system
 
